813019dc8a306f46a499c5b6b6494c1d4dade59e
Austin T <austin.taranto@gmail.com>
2020-07-15 15:19:56 -0400

    trying different times

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 document.getElementById("title").style.setProperty('--animate-duration', '2s');
 document.getElementById("left").style.setProperty('--animate-duration', '4s');
-document.getElementById("right").style.setProperty('--animate-duration', '5s');
+document.getElementById("right").style.setProperty('--animate-duration', '6s');
