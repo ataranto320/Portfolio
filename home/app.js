ac9aba38be50f0f892755a1631bd6284000b42d6
Austin T <austin.taranto@gmail.com>
2020-07-16 16:06:39 -0400

    tweaking austin appearing, animate link, js timing

---
+++ home/app.js
@@ -0,0 +1,6 @@
+document.getElementById("one").style.setProperty('--animate-duration', '2s');
+document.getElementById("two").style.setProperty('--animate-duration', '4s');
+document.getElementById("three").style.setProperty('--animate-duration', '6s');
+document.getElementById("four").style.setProperty('--animate-duration', '8s');
+document.getElementById("five").style.setProperty('--animate-duration', '10s');
+document.getElementById("six").style.setProperty('--animate-duration', '12s');
