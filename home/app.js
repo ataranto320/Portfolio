576add1b4122945476114f88b916d28553b09752
Austin T <austin.taranto@gmail.com>
2020-07-24 16:49:05 -0400

    trying to figure out why body background color isnt working and adjustments to time

--- home/app.js
+++ home/app.js
@@ -1,6 +1,6 @@
 document.getElementById("one").style.setProperty('--animate-duration', '2s');
-document.getElementById("two").style.setProperty('--animate-duration', '4s');
-document.getElementById("three").style.setProperty('--animate-duration', '6s');
-document.getElementById("four").style.setProperty('--animate-duration', '8s');
-document.getElementById("five").style.setProperty('--animate-duration', '10s');
-document.getElementById("six").style.setProperty('--animate-duration', '12s');
+document.getElementById("two").style.setProperty('--animate-duration', '3s');
+document.getElementById("three").style.setProperty('--animate-duration', '4s');
+document.getElementById("four").style.setProperty('--animate-duration', '5s');
+document.getElementById("five").style.setProperty('--animate-duration', '6s');
+document.getElementById("six").style.setProperty('--animate-duration', '7s');
