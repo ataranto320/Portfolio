4f9285be26f9280675f9b0eeb9a0514e1550e811
Austin T <austin.taranto@gmail.com>
2020-08-05 20:23:02 -0400

    got rid of old note

--- server.js
+++ server.js
@@ -13,6 +13,5 @@ app.use(express.json());
 // Routes
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
     res.sendFile(path.join(__dirname, "home.html"));
   });
